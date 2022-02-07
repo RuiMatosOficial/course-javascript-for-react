ce8e96e5be9feba1a40311409a1d533b8973df0d
Rui Matos <rui.microk@gmail.com>
2022-02-06 21:08:37 -0300

    Create  ScoreBoard object
    -  Add UI

--- main.js
+++ main.js
@@ -2,15 +2,14 @@ import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 import BoardGame from "./src/objects/BoardGame";
-import PlayerName from './src/components/PlayerName/index';
+import ScoreBoard from './src/objects/ScoreBoard/index';
 
 //  $nome_variável boas práticas colocar cifra antes variável tela
 const $root = document.querySelector("#root");
 $root.insertAdjacentHTML(
     'beforeend', 
-    `
-        ${PlayerName("Player1")}
-        ${PlayerName("Player2")}
+    `   
+        ${ScoreBoard()}
         ${BoardGame(6)}
     `
 );
