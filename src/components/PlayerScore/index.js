fa59b6d1dab0a03f20bf05e9e24f67b81b4ad7aa
Rui Matos <rui.microk@gmail.com>
2022-02-11 09:23:06 -0300

    Create HTML of the PlayerScore component

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,11 @@
+function PlayerScore(points = 0) {
+    return /* html */ `
+        <ol data-points="${points}">
+            <li>UM</li>
+            <li>DOIS</li>
+            <li>TRES</li>
+        </ol>   
+    `;
+}
+
+export default PlayerScore;
