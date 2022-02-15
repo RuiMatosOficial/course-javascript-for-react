662e3991276403d6d01e8139554a8e2431f0de64
Rui Matos <rui.microk@gmail.com>
2022-02-15 09:21:21 -0300

    Create component ArrowDown without CSS - Working in progress

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -2,13 +2,16 @@ import PlayerName from '../../components/PlayerName/index';
 import PlayerScore from '../../components/PlayerScore';
 import VsPlayer from '../../components/VsPlayer';
 import "./style.css";
+import ArrowDown from '../../components/ArrowDown/index';
+
 function ScoreBoard(){
     return /*html*/`
         <header class="score-board">
+            ${ArrowDown()}
             ${PlayerName("Player1")}
-            ${PlayerScore(3)}
+            ${PlayerScore(1)}
             ${VsPlayer()}
-            ${PlayerScore(2)}
+            ${PlayerScore(3)}
             ${PlayerName("Player2")}
         </header>
     `;
