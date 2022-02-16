bc7e31bb64c00e8025a0ca31c336c64c7ed60d20
Rui Matos <rui.microk@gmail.com>
2022-02-15 21:34:30 -0300

    Organization code in function

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,17 +3,42 @@ import CardFrontBack from "../../components/CardFrontBack";
 import cards from "./data.js";
 
 function BoardGame(amountCards) {
- const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon ));
 
- const $htmlCards = htmlCardsList.join('')
 
- console.log($htmlCards)
+  window.boardGame = {};
+  window.boardGame.handleClick = () => {
 
-  return /*html*/`
-    <section class="board-game">
-      ${$htmlCards}
-    </section>
-  `; 
+    const $boardGame = document.querySelector('.board-game');
+    const $arrowDown = document.querySelector('.arrow-down');
+    const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active');
+  
+    const flipAndHideCards = () => {
+      $cardsActive.forEach((card) => card.classList.remove('-active'));
+    }
+
+    const swapPlayer = (currentPlayer) => {
+      $arrowDown.setAttribute('data-currentPlayer', currentPlayer == 1 ? 2 : 1);
+    }
+
+    if ($cardsActive.length === 2) {
+      setTimeout(() => {
+        const currentPlayer = $arrowDown.getAttribute('data-currentplayer');
+        flipAndHideCards();
+        swapPlayer();
+      }, 1000);
+
+  
+    }
+  }
+  const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon ));
+
+  const $htmlCards = htmlCardsList.join('')
+
+    return /*html*/`
+      <section class="board-game" onClick="boardGame.handleClick(event)">
+        ${$htmlCards}
+      </section>
+    `; 
 }
 
 export default BoardGame;
